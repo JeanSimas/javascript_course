8f15f5b775df62e6decc2145069b8bb16fcf69bd
JeanSimas <jeansimas12@hotmail.com>
2019-06-05 17:53:06 -0300

    Adding the codingChallenge4

---
+++ codingChallenge4/script.js
@@ -0,0 +1,28 @@
+const john = {
+    fullName: "John Sauro",
+    mass: 75.3,
+    height: 1.75,
+    bmi: null,
+    calculateBMI: function () {
+        this.bmi = this.mass / this.height * this.height;
+        return this.bmi;
+    }
+};
+
+const mark = {
+    fullName: "Mark Sauro",
+    mass: 80.3,
+    height: 1.90,
+    bmi: null,
+    calculateBMI: function () {
+        this.bmi = this.mass / this.height * this.height;
+        return this.bmi;
+    }
+};
+
+
+john.calculateBMI();
+mark.calculateBMI();
+mark.bmi == john.bmi ? console.log(`${john.fullName} and ${mark.fullName} has the same BMI: ${john.bmi}`) :
+    mark.bmi > john.bmi ? console.log(`${mark.fullName} has the greater BMI: ${mark.bmi}`) :
+    console.log(`${john.fullName} has the greater BMI: ${john.bmi}`);
